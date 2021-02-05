f8df39904959a232359e1539d00252a413f22968
Juan Gallardo <juan.gallardo.ab@gmail.com>
2021-02-04 18:08:35 -0700

    Add replay option

--- app.py
+++ app.py
@@ -9,37 +9,45 @@ class CalculatorInputError(ValueError):
 def main_loop():
 	print("Welcome to the simple calculator. Please select from the following options:")
 
+	print()
+
 	print("1. Addition")
 	print("2. Subtraction")
 	print("3. Multiplication")
 	print("4. Division")
+
+	print()
 
 	user_selection = 0
 
 	while True:
 		try:
 			user_selection = int(input("Please enter your selection: "))
-			if (user_selection
-		< 1 or user_selection
-		> 4):
-				raise CalculatorInputError("Please enter a valid selection")
+			print()
+			if (user_selection < 1 or user_selection > 4):
+				raise CalculatorInputError()
 			else:
-				break	
-		except CalculatorInputError:
+				break
+		except ValueError as err:
+			print("\n**Please enter a valid selection**\n")
 			continue
 
 	while True:
 		try:
 			n1 = int(input("Please enter your first number: "))
+			print()
 			break	
 		except ValueError:
+			print("\n**Please enter a valid number**\n")
 			continue
 
 	while True:
 		try:
 			n2 = int(input("Please enter your second number: "))
+			print()
 			break	
 		except ValueError:
+			print("\n**Please enter a valid number**\n")
 			continue
 
 	if user_selection == 1:
@@ -54,5 +62,21 @@ def main_loop():
 	elif user_selection == 4:
 		division.divide(n1, n2)
 
-	main_loop()
+	print()
+
+	while True:
+		try:
+			response = input("Would you like to perform a new operation (y/n): ")
+			if (response != "y" and response != "n"):
+				raise CalculatorInputError("Please enter a valid selection")
+			else:
+				if (response == "y"):
+					print("\n**\n")		
+					main_loop()
+				else:
+					break	
+		except CalculatorInputError as err:
+			print(err)
+			continue
+	
 main_loop()
